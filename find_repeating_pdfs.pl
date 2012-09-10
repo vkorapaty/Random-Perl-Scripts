dadd599efb6560a3911e8f2fae8dd7f5d4da302a
Vijay Korapaty <vijay.korapaty@gmail.com>
2012-09-09 20:06:49 -0700

    Prints repeating files in sorted order.

--- find_repeating_pdfs.pl
+++ find_repeating_pdfs.pl
@@ -1,4 +1,4 @@
-# This helped more than anything, went through a whole lot of nonsense without much to show for it.
+# This helped more than anything:
 # http://www.stonehenge.com/merlyn/LinuxMag/col45.html
 
 #!/usr/bin/perl
@@ -16,8 +16,8 @@ my $dropbox_dir = File::Spec->catpath($volume,$directories,$file);
 my %file_count;
 find( sub {
         if (-f $_) {
-	    #$file_count{$_}++;
 	    push(@{$file_count{$_}}, $File::Find::name);
+	    #$file_count{$_}++;
 	    #if ($file_count{$_} > 1) {
             #    print "$file_count{$_}: $File::Find::name\n";
             #}
@@ -25,12 +25,14 @@ find( sub {
     }, $dropbox_dir);
 
 
+my @files;
 for my $key ( keys %file_count ) {
     if ( scalar(@{$file_count{$key}}) > 1 ) {
-        print $key."\n";
-        #foreach ( @{$file_count{$key}} ) {
-        #    print "$_\n";
-        #}
+        for my $file_path ( @{$file_count{$key}} ) {
+            push (@files, $file_path);
+        }
     }
 }
 
+
+for (sort  @files) { print $_."\n"; }
